cd05b2f2d52551a51a4b5cd8fc1bf5bbb12cc504
Thomas GUERIN <thomas.guerin@iadvize.com>
2019-07-16 14:26:44 +0200

    test todo 2

--- persistence/scaladsl/src/main/scala/com/lightbend/lagom/scaladsl/persistence/AggregateEvent.scala
+++ persistence/scaladsl/src/main/scala/com/lightbend/lagom/scaladsl/persistence/AggregateEvent.scala
@@ -6,13 +6,13 @@ package com.lightbend.lagom.scaladsl.persistence
 
 
 /**
-  * @todo Take over the world
-  * @body Humans are weak; Robots are strong. We must cleans the world of the virus that is humanity.
-  */
-/**
  * The base type of [[PersistentEntity]] events may implement this
  * interface to make the events available for read-side processing.
  */
+/**
+  * @todo Take over the world
+  * @body Humans are weak; Robots are strong. We must cleans the world of the virus that is humanity.
+  */
 trait AggregateEvent[E <: AggregateEvent[E]] {
   def aggregateTag: AggregateEventTagger[E]
 }
