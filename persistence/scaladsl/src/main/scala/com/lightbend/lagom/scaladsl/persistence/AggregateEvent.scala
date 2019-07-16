59a27d90ff342bc5a119ff906c1926da80a259b3
Thomas GUERIN <thomas.guerin@iadvize.com>
2019-07-16 14:25:27 +0200

    test todo 1

--- persistence/scaladsl/src/main/scala/com/lightbend/lagom/scaladsl/persistence/AggregateEvent.scala
+++ persistence/scaladsl/src/main/scala/com/lightbend/lagom/scaladsl/persistence/AggregateEvent.scala
@@ -4,6 +4,11 @@
 
 package com.lightbend.lagom.scaladsl.persistence
 
+
+/**
+  * @todo Take over the world
+  * @body Humans are weak; Robots are strong. We must cleans the world of the virus that is humanity.
+  */
 /**
  * The base type of [[PersistentEntity]] events may implement this
  * interface to make the events available for read-side processing.
